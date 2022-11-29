f87e04b6865002deafa14ed646b1a1cc284c6fc2
Marcus <marcusicaromc@gmail.com>
2022-11-29 14:22:57 -0300

    Finish app

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # weather-app
+
+Images:
+Clear Sky - Photo by <a href="https://unsplash.com/@fionn_lennon?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Fionn Lennon</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Thunderstorm - Photo by <a href="https://unsplash.com/@mantasos?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tasos Mansour</a> on <a href="https://unsplash.com/s/photos/thunderstorm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Drizzle - Photo by <a href="https://unsplash.com/@satyanto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hafidh Satyanto</a> on <a href="https://unsplash.com/s/photos/drizzle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Rain - Photo by <a href="https://unsplash.com/@kevin_w_?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kevin Wang</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Snow - Photo by <a href="https://unsplash.com/@sametomorrow?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Adam Chang</a> on <a href="https://unsplash.com/s/photos/Snow?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Clouded - Photo by <a href="https://unsplash.com/@iristimmermans?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Iris Timmermans</a> on <a href="https://unsplash.com/s/photos/Clouded?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Other - Photo by <a href="https://unsplash.com/@nedaastani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Neda Astani</a> on <a href="https://unsplash.com/s/photos/weather?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
---
+++ src/index.js
@@ -0,0 +1,64 @@
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=`
+const API_KEY = `&APPID=553668e995f051c41337492028b87231`;
+
+const city = document.getElementById('city');
+const cityBtn = document.getElementById('city-btn');
+const displayCurrentWeather = document.getElementById('current-weather');
+const displayCityName = document.getElementById('city-name');
+const displayCurrentDate = document.getElementById('current-date');
+const displayTemperature = document.getElementById('temperature');
+const displayTemperatureSwitch = document.getElementById('temperature-switch');
+const displayWeatherIcon = document.getElementById('weather-icon');
+const infoFeels = document.getElementById('feels');
+const infoHumidity = document.getElementById('humidity');
+// const infoChanceOfRain = document.getElementById('chance-of-rain');
+const infoWindSpeed = document.getElementById('wind-speed');
+
+// get weather data from the server
+async function getWeatherData (aCity) {
+    const response = await fetch(BASE_URL + aCity + API_KEY + "&units=metric");
+    const weatherData = await response.json();
+    displayCurrentWeather.textContent = weatherData.weather[0].main;
+    displayCityName.textContent = weatherData.name;
+    displayCurrentDate.textContent = new Date();
+    displayTemperature.textContent = weatherData.main.temp + "ºC";
+    infoFeels.textContent = weatherData.main.feels_like + 'ºC';
+    infoHumidity.textContent = weatherData.main.humidity + '%';
+    displayWeatherIcon.src = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"
+    infoWindSpeed.textContent = weatherData.wind.speed + 'm/s';
+}
+
+// get city data based on the input 
+const getCity = () => {
+    const cityName = city.value;
+    const convertedCityName = convertCityName(cityName);
+    getWeatherData(convertedCityName);
+}
+
+// start getCity on button press 
+cityBtn.addEventListener('click', getCity);
+
+// convert city name
+function convertCityName (someCity) {
+    const convertCityName = someCity.replace(/ /g, '+');
+    return convertCityName;
+}
+
+// switch temperature display
+function convertTemperature () {
+    const temperatureConvert = parseFloat(displayTemperature.textContent);
+    if (displayTemperature.textContent.includes('ºC')) {
+        const convertedToF = (temperatureConvert * 1.8) + 32;
+        displayTemperatureSwitch.textContent = 'Convert to ºC';
+        return displayTemperature.textContent = convertedToF.toFixed(2) + 'ºF';
+    } else if (displayTemperature.textContent.includes('ºF')) {
+        const convertedToCelsius = (temperatureConvert - 32) / 1.8;
+        displayTemperatureSwitch.textContent = 'Convert to ºF';
+        return displayTemperature.textContent = convertedToCelsius.toFixed(2) + 'ºC';
+    }
+}
+
+displayTemperatureSwitch.addEventListener('click', convertTemperature);
+
+// default city loader 
+getWeatherData('London');
